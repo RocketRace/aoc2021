699f809200a8853f4590bae95ac8539e9d2af643
Olivia Palmu <oliviaspalmu@gmail.com>
2021-12-07 17:45:38 +0200

    tweak build script

--- today.sh
+++ today.sh
@@ -13,8 +13,10 @@ fn entry(input: &_) -> usize {
 "
 [ -e "./input/$year/day$day.txt" ] || cargo aoc input
 [ -e "./src/y$year/d$day.rs" ] ||
-    echo $template > "./src/y$year/d$day.rs" &&
-    echo "mod d$day;" >> "./src/y$year/mod.rs"  &&
-    echo "Created template file"
+    (
+        echo $template > "./src/y$year/d$day.rs" &&
+        echo "mod d$day;" >> "./src/y$year/mod.rs"  &&
+        echo "Created template file"
+    )
 code -r "./src/y$year/d$day.rs"
 open "https://adventofcode.com/$year/day/$day"
